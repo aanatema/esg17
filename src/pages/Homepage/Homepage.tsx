e8cdf3acb954322202e491b03d96b95dcc1f6fc9
aanatema <romane.boireau@gmail.com>
2025-12-17 19:35:49 +0100

    add navbar to pages

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -1,3 +1,10 @@
+import Navbar from "@/components/Navbar";
+
 export default function Homepage() {
-	return <h1>Home page</h1>;
+	return (
+		<>
+			<Navbar />
+			<h1>Home page</h1>
+		</>
+	);
 }
--- src/pages/Services/Services.tsx
+++ src/pages/Services/Services.tsx
@@ -1,3 +1,10 @@
+import Navbar from "@/components/Navbar";
+
 export default function Services() {
-	return <h1>Services</h1>;
+	return (
+		<>
+			<Navbar />
+			<h1>Services</h1>
+		</>
+	);
 }
