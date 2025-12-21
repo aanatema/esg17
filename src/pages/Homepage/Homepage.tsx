6e64f01c8857fe939c7d33c06625a5872838328a
aanatema <romane.boireau@gmail.com>
2025-12-21 11:02:54 +0100

    update path following composant name change

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -1,10 +1,11 @@
-import Navbar from "@/components/Navbar";
+import DesktopNavbar from "@/components/Navbar/DesktopNavbar";
 
 export default function Homepage() {
 	return (
-		<>
-			<Navbar />
+		<main>
+			<DesktopNavbar />
+
 			<h1>Home page</h1>
-		</>
+		</main>
 	);
 }
--- src/pages/Pricing/Pricing.tsx
+++ src/pages/Pricing/Pricing.tsx
@@ -1,4 +1,4 @@
-import Navbar from "@/components/Navbar";
+import Navbar from "@/components/Navbar/DesktopNavbar";
 
 export default function Pricing() {
 	return (
