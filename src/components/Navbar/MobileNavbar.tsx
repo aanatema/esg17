70c3b066ac414f7a41cc652d8ba5cf809de1c090
aanatema <romane.boireau@gmail.com>
2025-12-21 12:10:12 +0100

    create mobile navbar

---
+++ src/components/Navbar/MobileNavbar.tsx
@@ -0,0 +1,38 @@
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "../ui/button";
+import { Link } from "react-router-dom";
+
+export default function MobileNavbar() {
+	return (
+		<div className='flex justify-center py-6'>
+			<DropdownMenu>
+				<DropdownMenuTrigger asChild>
+					<Button
+						variant='outline'
+						className='uppercase'>
+						Menu
+					</Button>
+				</DropdownMenuTrigger>
+				<DropdownMenuContent>
+					<DropdownMenuItem asChild>
+						<Link to='/'>Accueil</Link>
+					</DropdownMenuItem>
+					<DropdownMenuItem asChild>
+						<Link to='/prestations'>Prestations</Link>
+					</DropdownMenuItem>
+					<DropdownMenuItem asChild>
+						<Link to='/tarifs'>Tarifs</Link>
+					</DropdownMenuItem>
+					<DropdownMenuItem asChild>
+						<Link to='/contact'>Contact</Link>
+					</DropdownMenuItem>
+				</DropdownMenuContent>
+			</DropdownMenu>
+		</div>
+	);
+}
--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,9 +1,11 @@
 import Navbar from "@/components/Navbar/DesktopNavbar";
+import MobileNavbar from "@/components/Navbar/MobileNavbar";
 
 export default function Contact() {
 	return (
 		<>
 			<Navbar />
+			<MobileNavbar />
 			<h1>Contact</h1>
 		</>
 	);
