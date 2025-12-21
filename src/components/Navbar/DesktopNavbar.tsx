22903791ddb7c41850d92b92701f0b6f247bfcb2
aanatema <romane.boireau@gmail.com>
2025-12-21 11:02:37 +0100

    Change composant name to adapt to responsive design (desktop and mobile)

---
+++ src/components/Navbar/DesktopNavbar.tsx
@@ -0,0 +1,47 @@
+import {
+	NavigationMenu,
+	NavigationMenuItem,
+	NavigationMenuLink,
+	NavigationMenuList,
+	navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import { Link } from "react-router-dom";
+
+export default function DesktopNavbar() {
+	return (
+		<div className='flex justify-end py-2 px-6 bg-pink-200'>
+			<NavigationMenu>
+				<NavigationMenuList className='flex-wrap'>
+					<NavigationMenuItem>
+						<NavigationMenuLink
+							asChild
+							className={navigationMenuTriggerStyle()}>
+							<Link to='/'>Accueil</Link>
+						</NavigationMenuLink>
+					</NavigationMenuItem>
+					<NavigationMenuItem>
+						<NavigationMenuLink
+							asChild
+							className={navigationMenuTriggerStyle()}>
+							<Link to='/prestations'>Prestations</Link>
+						</NavigationMenuLink>
+					</NavigationMenuItem>
+					<NavigationMenuItem>
+						<NavigationMenuLink
+							asChild
+							className={navigationMenuTriggerStyle()}>
+							<Link to='/tarifs'>Tarifs</Link>
+						</NavigationMenuLink>
+					</NavigationMenuItem>
+					<NavigationMenuItem>
+						<NavigationMenuLink
+							asChild
+							className={navigationMenuTriggerStyle()}>
+							<Link to='/contact'>Contact</Link>
+						</NavigationMenuLink>
+					</NavigationMenuItem>
+				</NavigationMenuList>
+			</NavigationMenu>
+		</div>
+	);
+}
